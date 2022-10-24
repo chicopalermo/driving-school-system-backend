b7c6731bbd2843bfa27d1d631eb331f1c1bc6639
chicopalermo <franciscoctism@gmail.com>
2022-10-23 23:51:15 -0300

    config: cors config

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import { json, urlencoded } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import router from './router.js';
 
@@ -6,7 +8,17 @@ dotenv.config({path: '.env'});
 
 const app = express();
 
-app.use(express.json());
+app.disable('x-powered-by');
+
+const corsOptions = {
+    origin: "*",
+    methods: "GET,PUT,POST,DELETE",
+    allowedHeaders: "Content-Type,Authorization"
+}
+
+app.use(cors(null, corsOptions));
+app.use(json());
+app.use(urlencoded({ extended: true }));
 
 app.use(router);
 
